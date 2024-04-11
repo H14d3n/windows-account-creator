7b09f6b7841c547b8b48117e35b02a38ff7ca5ef
Tizian Imseng <tizian.imseng@bluewin.ch>
2024-04-11 10:39:29 +0200

    Update
    Add description to code

--- src/CustomTKinter/[CustomTKinter] Windows user creation.py
+++ src/CustomTKinter/[CustomTKinter] Windows user creation.py
@@ -22,27 +22,34 @@ def create_account():
 
 root = customtkinter.CTk()
 
+# Create Window for GUI
 root.title('Offline Account Creator')
 root.geometry('350x350')
 root.iconbitmap(r'C:\Users\2380\OneDrive - FernUni Schweiz\Dokumente\GitHub\tKinter-experiment\src\CustomTKinter\Pictogrammers-Material-Account.512.ico')
 
+# Title Account Creator centered
 title = customtkinter.CTkLabel(root, text="Account Creator", font=('Bold Calibri', 25))
 title.place(relx=0.25, rely=0.1)
 
+# Entry for Username
 username = customtkinter.CTkEntry(root, corner_radius=10, width=140, height=40, fg_color="#ffffff", text_color="#000000", placeholder_text="Username")
 username.place(relx=0.3, rely=0.3)
 
+# Entry for Password
 password = customtkinter.CTkEntry(root, corner_radius=10, width=140, height=40, fg_color="#ffffff", text_color="#000000", placeholder_text="Password")
 password.configure(show="*")
 password.place(relx=0.3, rely=0.45)
 
+# Checkbox if admin is needed
 admin_check_var = BooleanVar()
 admin_check = customtkinter.CTkCheckBox(root, corner_radius=10, fg_color="#ffffff", checkmark_color="#000000", text="Administrator", variable=admin_check_var)
 admin_check.place(relx=0.35, rely=0.625)
 
+# Function so that the button press works -> linked in command
 def submit_pressed():
     create_account()
 
+# Submit button, linked with submit_pressed function
 submit = customtkinter.CTkButton(root, corner_radius=10, text="Submit", fg_color="#ffffff", hover_color="#A9A9A9", text_color="#000000", command=submit_pressed)
 submit.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
 
